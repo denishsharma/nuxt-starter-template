d229344c10147d2322413c5b711da78e654b934f
Denish Sharma <denishsharma@ternaengg.ac.in>
2024-06-09 18:36:50 +0530

    feat(module): ✨ add nuxt seo module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
+import { isDevelopment } from "std-env";
+
 export default defineNuxtConfig({
     devtools: { enabled: true },
     app: {
         rootId: "__app",
-        head: {
-            title: "Nuxt Starter Template by Denish Sharma",
-        },
+    },
+    site: {
+        name: "Nuxt Starter Template",
+        description: "Nuxt 3 minimal starter template wih Vue 3, TypeScript, Unocss, SEO and more. Made by Denish Sharma",
     },
     typescript: {
         typeCheck: "build",
@@ -19,7 +22,11 @@ export default defineNuxtConfig({
         "@nuxt/eslint",
         "@unocss/nuxt",
         "@nuxt/fonts",
+        "@nuxtjs/seo",
     ],
+    seo: {
+        debug: isDevelopment,
+    },
     eslint: {
         config: {
             standalone: false,
